e5d76db510a21fceb343819147a2d625ffa2bce0
Edivaldo16 <edivaldojunior18@gmail.com>
2023-07-29 12:24:33 -0300

    Test Profile version 01

--- src/main/java/br/com/fuctura/biblioteca/profiles/TestProfile.java
+++ src/main/java/br/com/fuctura/biblioteca/profiles/TestProfile.java
@@ -1,5 +1,8 @@
 package br.com.fuctura.biblioteca.profiles;
 
+import br.com.fuctura.biblioteca.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -7,5 +10,12 @@ import org.springframework.context.annotation.Profile;
 @Profile("test")
 public class TestProfile {
 
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public void instaciaDB(){
+        this.dbService.instaciaDB();
+    }
 
 }
